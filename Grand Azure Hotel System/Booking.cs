e3546493bbfd022854d4ca92207e5c9ba39d866f
Sheika <sheika02x2@gmail.com>
2026-06-01 17:12:23 +0400

    add booking class

---
+++ Grand Azure Hotel System/Booking.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grand_Azure_Hotel_System
+{
+    public class Booking
+    {
+        private static int nextBookingID;
+        private int bookingID;
+        private Guest ? guest;
+        private Rooms ? room;
+        public int BookingID
+        {
+            get { return bookingID; }
+        }
+        public Guest ?Guest
+        {
+            get { return guest; }
+        }
+        public Rooms ?Room
+        {
+            get { return room; }
+        }
+        public Booking (Guest guest, Rooms room) 
+        {
+           this.guest = guest;
+           this.room = room;
+           bookingID = nextBookingID++;
+        }
+
+        public void DisplayBookingInfo()
+            {
+                Console.WriteLine("Booking ID: " + bookingID);
+                Console.WriteLine("Guest Name: " + guest?.FullName);
+                Console.WriteLine("Room Number: " + room?.RoomNumper);
+                Console.WriteLine("Room Type: " + room?.RoomType);
+            }
+    }
+}
