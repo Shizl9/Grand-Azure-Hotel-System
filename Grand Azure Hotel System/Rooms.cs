27af314b1fb5ec6fa932de605dba7505282a8f54
Sheika <sheika02x2@gmail.com>
2026-06-01 16:51:17 +0400

    add rooms class

---
+++ Grand Azure Hotel System/Rooms.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grand_Azure_Hotel_System
+{
+    public class Rooms
+    {
+        private int roomNumber;
+        private string? roomType;
+        private bool isBooked;
+        public int RoomNumper
+        {
+            get
+            {
+                return roomNumber;
+            }
+        }
+        public string? RoomType
+        {
+            get { return roomType; }
+        }
+        public bool IsBooked
+        {
+            get { return isBooked; }
+        }
+        public Rooms(int number, string type)
+        {
+            roomNumber = number;
+            roomType = type;
+            isBooked = false;
+        }
+        public bool Book()
+        {
+           
+        }
+        public void CancelBooking()
+        {
+            
+        }
+        public void DisplayRoomInfo()
+        {
+            Console.WriteLine("Room Number: " + roomNumber);
+            Console.WriteLine("Room Type: " + roomType);
+            Console.WriteLine("Is Booked: " + isBooked);
+        }
+        
+    }
+}
