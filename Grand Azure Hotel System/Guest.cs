602358a36f0b9af59c89891ee47db221967d3c09
Sheika <sheika02x2@gmail.com>
2026-06-01 13:42:03 +0400

    first commit

---
+++ Grand Azure Hotel System/Guest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grand_Azure_Hotel_System
+{
+    public class Guest
+    {
+        private static int totalGuestsCreated;
+        private string ?nationalID
+        {
+            get { return nationalID; }
+        }
+        private string ? fullName { get; set; }
+        public string ?NationalID 
+        { 
+            get { return nationalID; }
+        }
+        public string? FullName { get; set; }
+        public Guest(string name,string id)
+        {
+            FullName = name;
+            nationalID= id;
+            totalGuestsCreated++;
+        }
+
+        public static int GetTotalGuestsCreated()
+        {
+            return totalGuestsCreated;
+        }
+
+        public void DisplayGuestInfo()
+        {
+            Console.WriteLine("Guest Name: " + FullName);
+            Console.WriteLine("National ID: " + nationalID);
+        }
+
+    }
+}
